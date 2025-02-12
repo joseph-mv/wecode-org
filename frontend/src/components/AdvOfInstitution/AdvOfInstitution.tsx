82c18687a72d3a0ed68a423f7a90fa2c7726e6e8
joseph-mv <josephmv43@gmail.com>
2025-02-12 14:29:34 +0530

    style:Remove left and right button and arrange elements in the AdvofInstitution and BenefitsofTeam component

--- frontend/src/components/AdvOfInstitution/AdvOfInstitution.tsx
+++ frontend/src/components/AdvOfInstitution/AdvOfInstitution.tsx
@@ -1,37 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowsToDot,
-  faPerson,
-  faAnglesLeft,
-  faAnglesRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
-type Direction = "left" | "right";
+import { faArrowsToDot, faPerson } from "@fortawesome/free-solid-svg-icons";
+
 const AdvOfInstitution = () => {
   const icons = Array.from({ length: 8 });
-  const [tab, setTab] = useState<number>(1);
-  const [direction, setDirection] = useState<Direction>("left")
-
-//left, right button actions 
-  const handleTab = (direction: Direction) => { 
-    setDirection(direction)
-    if (direction === "left") setTab((prev) =>prev===1?prev=3: prev - 1);
-    else setTab((prev) =>prev%3 + 1);
-  };
 
   return (
     <div className="max-w-7xl mx-auto mt-10   font-lucida">
       <h1 className="text-center mb-2">ADVANTAGES FOR THE INSTITUTION</h1>
-      <div
-        className="relative  flex h-[500px] md:gap-14   "
-      >
+      <div className=" space-y-8 p-4 ">
         {/* EXPOSURE TO TOP TECH COMPANIES section */}
         <div
-          className={`relative  m-auto sm:max-w-[700px] sm:min-w-[700px] min-w-[100vw] h-[400px] overflow-hidden ${
-            tab !== 1 && "hidden" 
-          } 
-          ${direction==="left" && "animate-left2right" } 
-          ${direction==="right" && "animate-right2left" } 
+          className={`relative  m-auto sm:max-w-[700px] sm:min-w-[700px]  min-w-[100vw] h-[400px] overflow-hidden 
           block transition-all`}
         >
           <h2 className="text-center text-xl mb-14 font-trebuchet underline">
@@ -79,13 +58,7 @@ const AdvOfInstitution = () => {
           </div>
         </div>
         {/* PLACEMENTS section */}
-        <div
-          className={`relative overflow-hidden m-auto sm:max-w-[700px] animate-left2right sm:min-w-[700px] min-w-[100vw]  h-[400px] bg-black text-white p-2 ${
-            tab !== 2 && "hidden"
-          }  
-          ${direction==="left" && "animate-left2right" } 
-          ${direction==="right" && "animate-right2left" }  block`}
-        >
+        <div className="relative overflow-hidden m-auto sm:max-w-[700px]  sm:min-w-[700px] min-w-[100vw]  h-[400px] bg-black text-white p-2   block">
           <h2 className=" text-xl mb-8 font-trebuchet underline">PLACEMENTS</h2>
 
           <p className="w-[50%] ml-4">
@@ -120,14 +93,7 @@ const AdvOfInstitution = () => {
           </div>
         </div>
         {/* MORE EVENTS, MORE PLACEMENTS BRINGS MORE QUALITY STUDENTS */}
-        <div
-          className={`m-auto sm:min-w-[700px] min-w-[100vw]  h-[400px] animate-left2right  ${
-            tab !== 3 && "hidden"
-          }  
-           ${direction==="left" && "animate-left2right" } 
-          ${direction==="right" && "animate-right2left" } 
-          block`}
-        >
+        <div className="relative overflow-hidden m-auto sm:max-w-[700px]  sm:min-w-[700px] min-w-[100vw]  p-2   block">
           <h2 className=" text-xl font-trebuchet underline ml-2">
             MORE EVENTS, MORE PLACEMENTS BRINGS MORE QUALITY STUDENTS
           </h2>
@@ -139,7 +105,7 @@ const AdvOfInstitution = () => {
                 activities.
               </div>
               <div className="relative max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
-                DREAM 
+                DREAM
                 <img
                   className="absolute left-[80%]  w-[50%] -rotate-[80deg] object-contain z-10"
                   src="/images/arrow_bend_left_down_icon.png"
@@ -148,60 +114,43 @@ const AdvOfInstitution = () => {
               </div>
             </div>
 
-            {   <div className="space-y-4 mt-28">
-            <div className=" max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
-              <p >
-              Students will explore different paths to secure placements in
-                leading tech firms with the guidance and mentorship provided by
-                WeCode Club.
-              </p>
-                
+            {
+              <div className="space-y-4 mt-28">
+                <div className=" max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
+                  <p>
+                    Students will explore different paths to secure placements
+                    in leading tech firms with the guidance and mentorship
+                    provided by WeCode Club.
+                  </p>
+                </div>
+                <div className="relative max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
+                  <span>
+                    STRATEGIZE
+                    <img
+                      className="absolute left-[100%] bottom-0  w-[50%]  -rotate-[150deg]  object-contain"
+                      src="/images/arrow_bend_left_down_icon.png"
+                      alt=""
+                    />
+                  </span>
+                </div>
               </div>
-              <div className="relative max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
-                <span >
-                STRATEGIZE 
-                <img
-                  className="absolute left-[100%] bottom-0  w-[50%]  -rotate-[150deg]  object-contain"
-                  src="/images/arrow_bend_left_down_icon.png"
-                  alt=""
-                />
-                </span>
-               
-              </div>
-            </div>}
+            }
             <div className="space-y-4 mt-4">
               <div className="max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
-              <p > Students collaborate with professors and the Wecode community to
-                work on extraordinary projects, enhancing their profiles and
-                skills to excel in interviews.</p>
-               
+                <p>
+                  {" "}
+                  Students collaborate with professors and the Wecode community
+                  to work on extraordinary projects, enhancing their profiles
+                  and skills to excel in interviews.
+                </p>
               </div>
               <div className="max-w-44 w-[30vw] text-center bg-white rounded-md shadow-xl p-2">
-               <span  > ACHIEVE</span>
+                <span> ACHIEVE</span>
               </div>
             </div>
           </div>
         </div>
         {/* left button */}
-        <button
-          className={`absolute  left-[5vw] top-[90%] md:top-[50%]`}
-          onClick={() => handleTab("left")}
-        >
-          <FontAwesomeIcon
-            className="text-gray-600 text-4xl"
-            icon={faAnglesLeft}
-          />
-        </button>
-        
-        <button
-          className={`absolute right-[5vw] top-[90%] md:top-[50%] `}
-          onClick={() => handleTab("right")}
-        >
-          <FontAwesomeIcon
-            className="text-gray-600 text-4xl"
-            icon={faAnglesRight}
-          />
-        </button>
       </div>
     </div>
   );
